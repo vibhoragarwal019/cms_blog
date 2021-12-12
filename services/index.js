14884f6831839b5ff058a0e79c322b3bf6697ae5
vibhor <agarwalvibhor019@gmail.com>
2021-12-12 20:31:21 +0530

    error fix commit

--- services/index.js
+++ services/index.js
@@ -178,3 +178,39 @@ export const submitComment = async (obj) => {
   
     return result.posts;
   };
+  export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
